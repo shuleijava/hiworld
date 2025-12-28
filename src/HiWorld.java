0fe1920a61b7eca392c3166bc7ef3fc676b870a9
shuleijava <2294894345@qq.com>
2025-12-28 15:13:13 +0800

    test

--- src/HiWorld.java
+++ src/HiWorld.java
@@ -1,5 +1,5 @@
 public class HiWorld {
     public static void main(String args[]){
-        System.out.println("Hi World的双方各得1ddddddsaaaaaaaaaaadfqw任务共分为fs"";
+        System.out.println("Hi World的双方各得1ddddddsaaaaaaaaaaadfqw任务共分为fs");
     }
 }
